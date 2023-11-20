6ac616ff28793d773be5ed08009a3382ec15f14b
Alexand Shmel <amosemptines@yandex.ru>
2023-11-20 17:11:06 +0300

    correct arch

--- Search_engine/src/inverted_index.cpp
+++ Search_engine/src/inverted_index.cpp
@@ -1,27 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <map>
+#include "include/inverted_index.h"
 
 
-struct Entry {
-    size_t doc_id, count;
-
-    bool operator ==(const Entry& other) const {
-    return (doc_id == other.doc_id &&
-    count == other.count);
-    }
-};
-
-class InvertedIndex
+//Обновление базы документов
+void InvertedIndex::UpdateDocumentBase(std::vector<std::string> documents)
 {
-public:
-    InvertedIndex() = default;
+      docs = documents;
+      freq_dictionary.clear();
+      for (size_t doc_id = 0; doc_id < docs.size(); doc_id++) {
 
-    void UpdateDocumentBase(std::vector<std::string> input_docs);
+          const std::string& document = docs[doc_id];
+          std::map<std::string, size_t> word_count;
+          size_t start_pos = 0;
+          size_t end_pos = document.find(' ');
 
-    std::vector<Entry> GetWordCount(const std::string& word);
+          while (end_pos != std::string::npos) {
+            std::string word = document.substr(start_pos, end_pos - start_pos);
+            word_count[word]++;
+            start_pos = end_pos + 1;
+            end_pos = document.find(' ', start_pos);
+           }
+           std::string last_word = document.substr(start_pos);
+           word_count[last_word]++;
 
-private:
-    std::vector<std::string> docs;
-    std::map<std::string, std::vector<Entry>> freq_dictionary;
-};
+           for (const auto& pair : word_count) {
+               const std::string& word = pair.first;
+               size_t count = pair.second;
+               freq_dictionary[word].push_back({doc_id, count});
+           }
+       }
+}
+
+
+
